f3cbff81c44b09c66a1154d6292f00526be69952
Izpoid <142634517+Izpoid@users.noreply.github.com>
2023-11-07 23:27:48 -0300

    Add files via upload

---
+++ relogio digital/java.js
@@ -0,0 +1,11 @@
+function updateTime() {
+    const clock = document.getElementById('clock');
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    clock.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+updateTime(); // Chame a função uma vez para exibir a hora inicial
+setInterval(updateTime, 1000); // Atualize a cada segundo
